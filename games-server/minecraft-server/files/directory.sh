bb33939ca05cb848abb0e5e2e7b6849e0c48f9fc
flussence <gentoo+overlay@flussence.eu>
2015-07-30 22:32:50 +0100

    Remove CraftBukkit fossil
    This code hasn't supported it in recent memory, so there's no point having this
    workaround.

--- games-server/minecraft-server/files/directory.sh
+++ games-server/minecraft-server/files/directory.sh
@@ -12,26 +12,3 @@ gjl_pwd="$BASEDIR/${MULTIVERSE}"
 
 echo "Multiverse directory is ${gjl_pwd}." >&2
 mkdir -p "${gjl_pwd}"
-
-(
-    cd "${gjl_pwd}"
-    [[ -f "server.properties" ]] || exit
-
-    level_name=$(sed -n "s/^level-name=//p" "server.properties")
-
-    for D in "nether -1" "the_end 1"; do
-        TYPE="${D% *}"
-        DIM="DIM${D#* }"
-
-        if [[ -d "${level_name}" && -d "${level_name}_${TYPE}/${DIM}" ]]; then
-            echo -n "CraftBukkit ${TYPE} detected, " >&2
-
-            if [[ -d "${level_name}/${DIM}" && ! -L "${level_name}/${DIM}" ]]; then
-                echo "but a conflicting ${TYPE} is already present! Ignoring." >&2
-            else
-                echo "symlinking for the official server." >&2
-                ln -snf "../${level_name}_${TYPE}/${DIM}" "${level_name}/${DIM}"
-            fi
-        fi
-    done
-)
