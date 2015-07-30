ca5f3423f3198b2134df86875d690f7bf4e2247b
flussence <gentoo+overlay@flussence.eu>
2015-07-30 23:57:56 +0100

    Never write to filesystem from the launch script
    The initscript now does that for us, and this will now print an informative
    error if the directory it wants isn't there.

--- games-server/minecraft-server/files/directory.sh
+++ games-server/minecraft-server/files/directory.sh
@@ -1,14 +1,34 @@
-MULTIVERSE=${1:-main}
-[[ -z "$1" ]] && echo -n "Multiverse name not specified. "
-echo "Using multiverse name \"${MULTIVERSE}\"." >&2
+# Usage: minecraft-server-<$version> [$directory]
+# $directory may either be an existing directory path or a basename; if the
+# latter then the rest will be filled in based on the running user.
 
-if [[ "$(whoami)" == "minecraft-server" ]]; then
-    BASEDIR=${2:-/var/lib/minecraft}
+SERVER_NAME=${1:-main}
+[[ -z "$1" ]] && echo "Using default server name '$SERVER_NAME'."
+
+mc_uid='minecraft-server'
+
+if [[ -d "$SERVER_NAME" ]]; then
+    gjl_pwd=$SERVER_NAME
+elif [[ "$(whoami)" == "$mc_uid" ]]; then
+    gjl_pwd="$HOME/$SERVER_NAME"
+    err_cmd="# install -m 0750 -o $mc_uid -g $(id -ng $mc_uid) -d '$gjl_pwd'"
 else
-    BASEDIR="${HOME}/.minecraft/servers"
+    gjl_pwd="$HOME/.minecraft/servers/$SERVER_NAME"
+    err_cmd="$ mkdir -p '$gjl_pwd'"
 fi
 
-gjl_pwd="$BASEDIR/${MULTIVERSE}"
+echo "Server data directory set to '$gjl_pwd'."
 
-echo "Multiverse directory is ${gjl_pwd}." >&2
-mkdir -p "${gjl_pwd}"
+if [[ ! -d "$gjl_pwd" ]]; then
+    echo
+    echo "ERROR: Data directory does not exist."
+    echo "This launcher script will not create it automatically for security"
+    echo "reasons. You can do it manually using the following command, but"
+    echo "verify its sanity first:"
+    echo "$err_cmd"
+    echo
+    echo "Remember that newer versions of the server will not run until after"
+    echo "you manually accept the EULA, which it writes to the data directory."
+    exit 1
+fi
+
